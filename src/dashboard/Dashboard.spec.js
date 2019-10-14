d795916e09befec9ee5908e2450605a7c9458e4b
Uzias Rivera <uziasrivera@gmail.com>
2019-10-14 13:45:00 -0700

    working on tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from 'react'
+import renderer from 'react-test-renderer';
+import Dashboard from './Dashboard'
+import { render, fireEvent } from '@testing-library/react';
+
+test('it renders correctly', () => {
+    // do snapshots here if you're not in codesandbox
+    render(<Dashboard />);
+  });
+  
+
+it('renders correctly',()=>{
+    const tree = renderer
+    .create(<Dashboard/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot()
+})
